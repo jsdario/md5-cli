07474b9ff8d68a3ce41afbb72b9c0bd7d48cf006
Terrence Wong <wongterrencew@gmail.com>
2016-07-07 16:09:03 +0200

    optionally read from stdin

--- index.js
+++ index.js
@@ -5,7 +5,25 @@ var fs = require('fs');
 var md5 = require('md5');
 var path = require('path');
 
-var target = process.argv[2];
-var pathToTarget = path.resolve(__dirname, target);
+// Example: md5-cli 'foo'
+if (process.stdin.isTTY) {
+  var target = process.argv[2];
+  console.log(md5(target));
 
-console.log(md5(target));
+// Example: echo 'foo' | md5-cli
+} else {
+  var data = '';
+   process.stdin.on('readable', function() {
+    var chunk;
+    while (chunk = process.stdin.read()) {
+      data += chunk;
+    }
+  });
+
+  process.stdin.on('end', function () {
+    // There will be a trailing \n from the user hitting enter. Get rid of it.
+    data = data.replace(/\n$/, '');
+    console.log(md5(data));
+  });
+}
+
